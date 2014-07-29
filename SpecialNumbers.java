4c81b3e65b45980ac01cd165e598b4a2f49f0f0b
siddharth-shah <shah.sgsits@gmail.com>
2014-07-29 18:39:49 +0530

    Create SpecialNumbers.java

---
+++ SpecialNumbers.java
@@ -0,0 +1,13 @@
+public  interface SpecialNumbers {
+
+	// First element is empty for simplifying indexing 
+	public static final String[] UNITS = { "", "one", "two", "three", "four",
+			"five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+			"thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+			"eighteen", "nineteen" };
+
+	// First two elements are empty for simplifying indexing 
+	public static final String[] TENS = { "", "", "twenty", "thirty", "forty",
+			"fifty", "sixty", "seventy", "eighty", "ninety" };
+
+}
